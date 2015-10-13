36afe435f0a5fb10d024c411958256edb25bfdaf
Sean Beck <spcbeck@gmail.com>
2015-10-12 23:55:46 -0400

    Attempting to get app working on heroku.

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
 app.use('/', routes);
